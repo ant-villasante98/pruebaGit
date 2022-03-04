f2de92159629b3370c601c09c9ece0ee5c138d7e
Antonio Villasante Hilares <hilare33v@gmail.com>
2022-03-04 18:55:34 -0300

    quitando peso

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './style.css'
 
-import "@fortawesome/fontawesome-free/js/all.js";
+// import "@fortawesome/fontawesome-free/js/solid.js";
+import "@fortawesome/fontawesome-free/js/solid.js";
 import "@fortawesome/fontawesome-free/css/all.css";
 
 import './component/calendario/calendario-component.css'
