13362239cab3b867ab7eb72f051e65fc54b1f19a
Antonio Villasante Hilares <hilare33v@gmail.com>
2022-03-05 01:23:19 -0300

    favicon agregado

--- src/index.js
+++ src/index.js
@@ -2,28 +2,6 @@ import './style.css'
 import './component/calendario/calendario-component.css';
 
 import './component/calendario/calendario-component.ts';
-// import { library, icon } from '@fortawesome/fontawesome-svg-core/index.js'
-// import { faAngleLeft } from '@fortawesome/free-solid-svg-icons'
-
-// library.add(faAngleLeft)
-
-// const angleLeft = icon({ prefix: 'fas', iconName: 'angle-left' })
-// console.log(faAngleRight)
-
-// document.querySelector('.font-here').innerHTML = faAngleRight;
-
-
-
-// init();
-
-
-// getEvent()
-// .then((res) => res.json())
-//     .then(resJson => {
-//         let events = resJson.items;
-        
-//     })
-
 
     
 
